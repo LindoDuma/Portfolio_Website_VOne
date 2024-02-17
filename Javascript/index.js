0c2b9045ace2fdc9ee215d4315d78461e2a0d12b
Lindokuhle Duma <lindoduma10@gmail.com>
2024-02-17 18:32:54 +0200

    Solve background ball positioning and size

---
+++ Javascript/index.js
@@ -0,0 +1,5 @@
+var headImage = document.querySelector("#image");
+
+function HideImage(){
+    headImage.classList.add("hidden");
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,18 @@
-module.exports = {   content: ['./src/**/*.{html,js}'],   theme: {}, };
+module.exports = {   content: ['./src/**/*.{html,js}'],   theme: {
+    extend: {
+      fontFamily: {
+        NotoSans: ["Noto Sans", "san-serif"],
+        Roboto: ["Roboto", "san-serif"],
+        //More Fonts Later
+      },
+      colors:{
+        "primary-color":"#00FF00",
+        "secondary-color":"#00FF00",
+        "tertiary-color":"#00FF00",
+        "quartiary-color":"#00FF00"
+      },
+      screens:{
+        "sm":"480px"
+      }
+    },
+  }, };
