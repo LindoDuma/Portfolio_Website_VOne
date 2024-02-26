dc7a8907a0a1473f6f70dbdca77a700ebb66a06a
Lindokuhle Duma <lindoduma10@gmail.com>
2024-02-26 20:05:56 +0200

    contact section touch up and some responsiveness

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,5 +1,12 @@
 var headImage = document.querySelector("#image");
+const icon = document.querySelector("#item");
 
 function HideImage(){
     headImage.classList.add("hidden");
+}
+function Open(){
+    icon.classList.remove("hidden");
+}
+function Close(){
+    icon.classList.add("hidden");
 }
