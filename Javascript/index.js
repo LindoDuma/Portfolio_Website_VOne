af426855520152435daa7cd5a3f5f07e8fb8dfe2
Lindokuhle Duma <lindoduma10@gmail.com>
2024-06-28 13:35:08 +0200

    added animation using intersection observer

--- Javascript/index.js
+++ Javascript/index.js
@@ -30,3 +30,17 @@ const CloseProjectDetails = (currentProject) => {
     currentProject.children[1].classList.add('md:-top-40');
     currentProject.children[1].classList.remove('md:top-0');
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else{
+            entry.target.classList.remove('show');
+        }
+    }))
+})
+
+const hiddenElements = document.querySelectorAll('.hide,.hide-right,.hide-fade');
+hiddenElements.forEach((el) => observer.observe(el));
